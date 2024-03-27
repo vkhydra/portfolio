5f0515f72a9937dc1c272a1ea0235a37f8abb0b7
Victor Bruno Kulessa <victor@kulessa.com.br>
2024-03-26 22:44:44 -0300

    Update header component with new text styles and additional information

--- components/cabecalho.tsx
+++ components/cabecalho.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 export default function Cabecalho() {
   return (
     <div className="py-16 px-4 max-w-screen-xl mx-auto">
-      <span className="uppercase font-bold text-info md:text-xl lg:text-2xl">
+      <span className="uppercase font-bold text-info text-lg md:text-xl lg:text-2xl">
         Hello, my name is Victor
       </span>
-      <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold">
-        I&apos;m a software developer
+      <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold font-serif">
+        I develop web applications.
       </h1>
+      <h2 className="md:text-lg lg:text-2xl mt-2">
+        I am always looking for new challenges for my career to learn and grow
+        as a professional.
+      </h2>
     </div>
   );
 }
